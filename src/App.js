fd97279dd19f280eb49d13df6c4b90f54fb28a29
Kisha Prudente <kisha.prudente@gmail.com>
2020-09-07 23:22:36 -0400

    import Dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className='App'>
+      <Dashboard />
     </div>
   );
 }
