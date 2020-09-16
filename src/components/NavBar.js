c7d52013dffa86e0bba1e45a7d76b4214918c558
Kisha Prudente <kisha.prudente@gmail.com>
2020-09-16 18:42:48 -0400

    added search bar to header

---
+++ src/components/NavBar.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import { AppBar, Typography, Toolbar, InputBase, IconButton } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import API from '../util/API';
+
+const useStyles = makeStyles((theme) => ({
+  nav: {
+    backgroundColor: '#136D9D'
+  }, 
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: '20px',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(10),
+      width: '55%',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'white',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(2em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  const [searchCity, setSearchCity] = useState('');
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setSearchCity(value);
+  }
+
+  const handleKeyPress = (e) => {
+    const { value } = e.target;
+    if(e.keyCode === 13) {
+      console.log('city:', value);
+      handleSearch(searchCity);
+    }
+  }
+
+  const handleSearch = async (city) => {
+    const weatherData = await API.getCityData(city);
+    console.log(weatherData);
+  }
+
+  return (
+    <AppBar position="static" className={classes.nav}>
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Weather Dashboard
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search City…"
+              onChange={(e) => handleInputChange(e)}
+              onKeyDown={(e) => handleKeyPress(e)}
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+  )
+}
+
+export default NavBar;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-// import styled from 'styled-components';
-import Hero from '../components/Hero';
-// import SearchBar from '../components/SearchBar';
-
-// const GridContainer = styled(Grid)`
-//   justify-content: center;
-//   width: 100%;
-//   height: 100%;
-// `;
+import NavBar from '../components/NavBar';
 
 const Dashboard = () => {
   return (
     <Grid container style={styles.root}>
-      <Grid item xs={10}>
-        <Hero />
+      <Grid item xs={12}>
+        <NavBar />
       </Grid>
     </Grid>
   );
