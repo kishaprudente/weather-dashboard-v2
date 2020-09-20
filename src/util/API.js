6d01148ef62d0ae35e3619b34dd177d3c37413a8
Kisha Prudente <kisha.prudente@gmail.com>
2020-09-20 00:42:47 -0400

    changed unit to Fahrenheit

--- src/util/API.js
+++ src/util/API.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 export default {
   getCityData: (city) => {
-    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OWM_API_KEY}`;
+    const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`;
     return axios.get(queryURL);
   },
 };
