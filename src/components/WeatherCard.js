a9e68b841175b4315141faf59bc8d2817865bec6
Kisha Prudente <kisha.prudente@gmail.com>
2020-09-20 00:43:09 -0400

    added card for weather details

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Paper, Card, CardContent, Typography } from '@material-ui/core';
+
+const WeatherCard = ({name, temp, feelsLike, pressure, humidity, min, max}) => {
+  return (
+    <Paper>
+      <Card>
+        <CardContent>
+          <Typography>{temp} °F</Typography>
+        </CardContent>
+      </Card>
+    </Paper>
+  )
+};
+
+export default WeatherCard;
